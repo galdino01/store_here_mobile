217ff6e11bafecd1632348601d505d638e3a80b7
galdino01 <lucaspolucena@hotmail.com>
2022-08-12 14:36:57 -0300

    Improving FormLogin responsiveness and adding email validation

--- lib/main.dart
+++ lib/main.dart
@@ -29,61 +29,97 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class FormLogin extends StatelessWidget {
+class FormLogin extends StatefulWidget {
   const FormLogin({Key? key}) : super(key: key);
 
   @override
+  FormLoginState createState() {
+    return FormLoginState();
+  }
+}
+
+class FormLoginState extends State<FormLogin> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(25),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Container(
-          margin: const EdgeInsets.fromLTRB(0, 0, 0, 50),
-          child: const Text(
-            'Acesse sua conta',
-            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 50),
+                    child: const Text(
+                      'Acesse sua conta',
+                      style:
+                          TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Digite seu email',
+                        prefixIcon: Icon(Icons.alternate_email)),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'O campo email não pode estar vazio!';
+                      }
+                      if (!RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value)) {
+                        return 'Digite o email corretamente! Ex: usuario.mundo@email.com';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                    onChanged: (String? value) {},
+                    decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Digite sua senha',
+                        prefixIcon: Icon(Icons.password)),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Esqueci minha senha',
+                      style: TextStyle(color: Colors.blue, fontSize: 12),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 150,
+                    margin: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, or false otherwise.
+                        if (_formKey.currentState!.validate()) {
+                          // If the form is valid, display a snackbar. In the real world,
+                          // you'd often call a server or save the information in a database.
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Carregando...')),
+                          );
+                        }
+                      },
+                      child:
+                          const Text('Login', style: TextStyle(fontSize: 24)),
+                    ),
+                  ),
+                ]),
           ),
         ),
-        TextFormField(
-          decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Digite seu email',
-              prefixIcon: Icon(Icons.alternate_email)),
-        ),
-        TextFormField(
-          obscureText: true,
-          obscuringCharacter: '*',
-          onChanged: (String? value) {},
-          decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Digite sua senha',
-              prefixIcon: Icon(Icons.password)),
-        ),
-        TextButton(
-          onPressed: () {},
-          child: const Text(
-            'Esqueci minha senha',
-            style: TextStyle(color: Colors.blue, fontSize: 12),
-          ),
-        ),
-        Container(
-          height: 50,
-          width: 250,
-          margin: const EdgeInsets.fromLTRB(0, 50, 0, 0),
-          decoration: BoxDecoration(
-              color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-          child: TextButton(
-            onPressed: () {},
-            child: const Text(
-              'Login',
-              style: TextStyle(
-                  color: Colors.white,
-                  backgroundColor: Colors.blue,
-                  fontSize: 25),
-            ),
-          ),
-        )
-      ]),
+      ),
     );
   }
 }
